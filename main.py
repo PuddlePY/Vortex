9186ab317e580287e9392231ce9e7796b119afd3
Puddles-pngfile <thomas.munday23@gmail.com>
2021-04-18 15:28:15 +0100

    About Page
    about the page abouts the page

--- main.py
+++ main.py
@@ -11,5 +11,19 @@ app = Flask(__name__)
 
 #app routes
 @app.route("/")
-def home:
+def home():
     return render_template("home.html")
+
+
+@app.route("/aboutus")
+def about():
+    return render_template("about.html")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
