c82ceec97726d17cdfdc1a000160a1e818d002c9
Puddles-pngfile <thomas.munday23@gmail.com>
2021-04-18 15:04:43 +0100

    commit3

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
+#imports
 
 
 
+#flask app
 app = Flask(__name__)
 
 
 
 
-
+#app routes
 @app.route("/")
 def home:
+    return render_template("home.html")
